ff782ed0e849d16bc70075d0fc172521e4bd2389
krijesnica96 <1996role@gmail.com>
2018-01-06 20:48:08 +0100

    Update accountant.js

--- service/accountant.js
+++ service/accountant.js
@@ -1,26 +1,58 @@
+const connection = require('../db/db.js');
+const Promise = require('promise');
+
 class accountant {
 
 	accountant(){
 	
 	};
 
-	checkIllegalChar(const data,callback){
+	checkIllegalChar(data,callback){
 	
-	if(data == NULL) return callback('Wrong input');
+		if(data == null) return callback('Wrong username input');
 	
-	for(let i=0;i<data.length;i++){
+		for(let i=0;i<data.length;i++){
 
-		if((uname[i]<'A' || uname[i]>'Z')&&(uname[i]<'a' || uname[i]>'z')){
+			if((data[i]<'A' || data[i]>'Z')&&(data[i]<'a' || data[i]>'z')){
 			
-			return callback('Username contains illegal char');
+				return callback('Username contains illegal char');
+			}
+		
 		}
 		
-	}
-	
-	return callback(NULL,data);
+		return callback(null);
 	
 	};
 
-}
+	checkSpacePassword(data,callback){
+		
+		for(let i=0;i<data.length;i++){
+			
+			if(data==null) return callback('Wrong password input');
+		
+			if(data[i]==' '){
+			
+				return callback('Password containts space');
+			
+			}
+		}
+		
+		return callback(null);
+		
+	};
+	
+	checkRePassword(first,second,callback){
+		
+		if(first == null) return callback('The first password is invaild');
+		
+		if(second == null) return callback('The second password is invalid');
+		
+		if(first===second) return callback(null);
+		
+		return callback('Your passwords don\'t match');
+		
+	};	
+	
+};
 
 module.exports = new accountant();
