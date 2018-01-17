9ee2cd15e40935a34fb1d67beea658da1870c993
krijesnica96 <1996role@gmail.com>
2018-01-17 22:21:23 +0100

    Update search.js

--- routes/search.js
+++ routes/search.js
@@ -3,11 +3,14 @@ const router = express.Router();
 const connection = require('../db/db.js');
 const socket = require('../routes/sockets.js');
 const dbHandler = require('../service/dbHandler.js');
+const Promise = require('promise');
 
 router.get('/', function(req, res, next) {
 	
-	res.render('search',{res:5});
+	if(!req.session.id) res.redirect('login');
 	
+	res.render('search',{res:5});
+
 	socket.on('connection',function(client){
 		
 		client.on('search',function(value){
@@ -24,8 +27,30 @@ router.get('/', function(req, res, next) {
 
 router.post('/', function(req,res,next){
 	
-	console.log(req.body);
+	const promise = new Promise(function(){
+		
+		dbHandler.likeAd(req.body,req.session.id,function(err,results){
+			
+			if(err){
+				
+				res.status(500).json(err);
+				return;
+			
+			}
+		});
+	}).then(
 	
+	dbHandler.updateLikes(req.body,function(err,results){
+		
+		if(err){
+			
+			res.status(500).json(err);
+			return;
+		}
+		
+		res.redirect('/');
+		
+	}));
 });
 
 module.exports = router;
