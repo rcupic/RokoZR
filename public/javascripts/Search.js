f771db32b184f517925bbaa2931bea7bf737b6eb
krijesnica96 <1996role@gmail.com>
2018-01-17 09:51:10 +0100

    Update Search.js

--- public/javascripts/Search.js
+++ public/javascripts/Search.js
@@ -1,14 +1,18 @@
 function Search(){
-
-	var socket = io('http://localhost:5000');
+	const socket = io('http://localhost:5000');
 	
-	var txt = document.getElementById('searchInput').value;
+	const txt = document.getElementById('searchInput').value;
 	
 	socket.emit('search',txt);
 	
 	socket.on('finishedSearch',function(res){
 		
-		document.getElementById('pSearch0').innerHTML = res[0].name+","+res[0].description;
-				
+		let i = 0;
+		while(i<res.length){
+			console.log(res[i].description);
+			document.getElementById('pSearch'+i).innerHTML = "<p>"+res[i].name+"</p></br><p>"+res[i].description+"</p></br><input type='text' name='amount' placeholder='Amount'></br><input type='radio' name='id' value='"+res[i].idad+"'>";
+			i++;
+		 
+		}		
 	});
 }
