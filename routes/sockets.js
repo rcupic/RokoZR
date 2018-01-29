7b3666de5ed26b805276118c819cc414462cfe3f
krijesnica96 <1996role@gmail.com>
2018-01-29 13:31:24 +0100

    Update sockets.js

--- routes/sockets.js
+++ routes/sockets.js
@@ -1,7 +1,7 @@
-var http = require('http');
-var server = http.createServer(function(){
+const http = require('http');
+const server = http.createServer(function(){
 
 });
-var socket = require('socket.io')(server);
+const socket = require('socket.io')(server);
 server.listen(5000);
 module.exports=socket;
