599b930f13e75359f85b82bc00c3451f785d8e52
krijesnica96 <1996role@gmail.com>
2018-01-29 13:29:36 +0100

    Update secure.js

--- routes/secure.js
+++ routes/secure.js
@@ -1,13 +1,23 @@
 const express = require('express');
 const router = express.Router();
+const dbHandler = require('../service/dbHandler.js');
 
 router.get('/',function(req,res,next){
 	
-	if(req.session.username){//CHECK IF USER IS LOGGED IN
+	if(req.session.id){//CHECK IF USER IS LOGGED IN
 		
-		console.log(req.session);
-		res.render('secure',{name: req.session.username,heart: req.session.hearts,rank: req.session.rank});
-	
+		dbHandler.searchForUser(req.session.id,function(err,results){
+			
+			if(err){
+				
+					res.status(500).json(err);
+					return;
+			}
+			
+			res.render('secure',{name: results[0].username,heart: results[0].hearts,rank: results[0].rank});
+			
+		});
+			
 	}else{
 		
 		res.redirect('/login');
