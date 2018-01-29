4d4c619e8074187acae4b739805f96d9e13dda88
krijesnica96 <1996role@gmail.com>
2018-01-29 13:27:33 +0100

    Update newAd.js

--- routes/newAd.js
+++ routes/newAd.js
@@ -4,14 +4,8 @@ const connection = require('../db/db.js');
 
 router.get('/',function(req,res,next){
 
-	res.render('newAd');
-
-});
-
-router.post('/',function(req,res,next){
-		
 	//SQL QUERY FOR CHECKING ACTIVE AD
-	let sql = "SELECT idad FROM usersdb.users,usersdb.ads WHERE users.username='"+req.session.username+"' AND ads.active=1;";
+	let sql = "SELECT idad FROM usersdb.users,usersdb.ads WHERE users.iduser='"+req.session.id+"' AND users.iduser = ads.iduser AND ads.active=1;";
 
 	connection.query(sql,function(err,results,fields){//CHECK IF THERE IS ALREADY ACTIVE AD
 		
@@ -20,6 +14,16 @@ router.post('/',function(req,res,next){
 			res.redirect('/myAd');
 			
 		}else{
+			
+			res.render('newAd');
+			
+		}
+	});
+});
+
+router.post('/',function(req,res,next){
+		
+	
 			//SQL QUERY FOR CREATING NEW AD
 			sql = "INSERT INTO usersdb.ads(iduser,cathegory,name,value,description) VALUES ("+req.session.id+",'"+req.body.adCathegory+"','"+req.body.adName+"',"+req.body.adAmount+",'"+req.body.adDescription+"');";
 						
@@ -27,10 +31,6 @@ router.post('/',function(req,res,next){
 				//THEN SERVE CLIENT HIS AD
 				res.render('myAd',{name: req.body.adName,value: req.body.adAmount,likes: 0,cathegory: req.body.adCathegory,description: req.body.adDescription});
 				
-			})
-		}
-	})
+			});
 });
-
-
 module.exports = router;
