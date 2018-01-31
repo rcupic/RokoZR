acd8dc9c07ab24edd301bc0a81165416197e4463
krijesnica96 <1996role@gmail.com>
2018-01-31 11:26:23 +0100

    Update myAd.js
    Moved logic to dbHandler

--- routes/myAd.js
+++ routes/myAd.js
@@ -1,27 +1,26 @@
 const express = require('express');
 const router = express.Router();
-const socket = require('../routes/sockets.js');
-const connection = require('../db/db.js');
 const dbHandler = require('../service/dbHandler.js');
 const Promise = require('promise');
 
 router.get('/',function(req,res,next){
 	
-	let sql = "SELECT * FROM usersdb.ads,usersdb.users WHERE usersdb.users.iduser="+req.session.id+" AND usersdb.users.iduser = usersdb.ads.iduser AND usersdb.ads.active = 1";
-	
-	connection.query(sql,function(err,results,fields){//TREBA PROVJERITI PRIJE IMA LI UOPCE TRENUTNE REKLAME
-
-		if(results.length){
+	dbHandler.getMyAd(req.session.id,function(err,results){
+		
+		if(err){
 			
-			const stringRes = JSON.parse(JSON.stringify(results));
-			res.render('myAd',{name: stringRes[0].name,likes: stringRes[0].likes,value: stringRes[0].value,cathegory: stringRes[0].cathegory, description: stringRes[0].description});
+			console.log(err);
+			return;
+			
+		}else if(results){
+			
+			res.render('myAd',{name: results.name,likes: results.likes,value: results.value,cathegory: results.cathegory, description: results.description});
 		
 		}else{
 			
 			res.redirect('/secure');
 			
 		}
-		
 	});
 });
 
@@ -33,11 +32,11 @@ router.post('/',function(req,res,next){
 			
 			if(err){
 				
-				res.status(500).json(err);
+				console.log(err);
 				return;
 			
 			}
-			console.log('1');
+			
 		});
 	}).then(
 		
@@ -45,22 +44,22 @@ router.post('/',function(req,res,next){
 			
 			if(err){
 				
-				res.status(500).json(err);
+				console.log(err);
 				return;
 				
 			}
-			console.log('2');
+			
 		})	
 	).then(
 		dbHandler.deactAd(req.session.id,function(err,results){
 			
 			if(err){
 				
-				res.status(500).json(err);
+				console.log(err);
 				return;
 				
 			}
-			console.log('3');
+			
 			res.redirect('myAd');
 		})
 	);
