03270137b3d33e95babae03dbf5f8205e0dd9f10
krijesnica96 <1996role@gmail.com>
2018-01-31 03:23:07 +0100

    Update dbHandler.js
    All functions now have 2 parameters. I sorted functions, with comments, by their functionalities.

--- service/dbHandler.js
+++ service/dbHandler.js
@@ -6,77 +6,8 @@ class dbHandler{
 	dbHandler(){
 	
 	};
-	
-	addNewUser(data,callback){
-		
-		let sql = 'INSERT INTO usersdb.users (username, password, email)VALUES (\''+data.usrname+'\',\''+data.password+'\',\''+data.email+'\');'
-		
-		connection.query(sql,function(err,results,fields){
-
-			
-			if(results.length==0) return callback('No changes made in db!');
-
-			return callback(null,results);
-		});
-		
-	};
-	
-	checkPassword(data,callback){
-		
-		const sql = 'SELECT * FROM usersdb.users WHERE users.password = "'+data+'";';
-		
-		connection.query(sql,function(err,results,fields){
-		
-		if(results.length == 0) return callback('Wrong password');
-		
-		return callback(null,results);
-		
-		});
-	};
-	
-	collectCoins(data,callback){
-		console.log(data);
-		const sql = 'UPDATE usersdb.users SET hearts = hearts + (SELECT SUM(likes) FROM usersdb.ads WHERE users.iduser='+data+' AND users.iduser = ads.iduser AND ads.active = 1) WHERE users.iduser='+data+';';
-		
-		connection.query(sql,function(err,results,fields){
-			
-			if(results.length == 0) return callback('Error collecting coins!');
-			
-			return callback(null,results);
-			
-		});
-		
-	};
-	
-	deactAd(data,callback){
-		
-		const sql = 'UPDATE usersdb.ads SET active = 0 WHERE ads.iduser = '+data+' AND ads.active = 1;';
-		
-		connection.query(sql,function(err,results,fields){
-			
-			if(results.lentgh ==0) return callback('Error deactivation ad!');
-			
-			return callback(null,results);
-		});
-		
-	};
-	
-	checkForMail(data,callback){
-		
-		if(data == null) return callback('Wrong mail input!');
-		
-		let sql = "SELECT iduser FROM usersdb.users WHERE usersdb.users.email='"+data+"'";
-		
-		connection.query(sql,function(err,results,fields){
-
-			
-			if(results.length!=0) return callback('Mail is in use!');
-
-			return callback(null);
-		});
-	};
-	
-	checkForName(data,callback){
+	//REGISTRATION---------------------------------------------------------------------------------------------------------
+	checkForName(data,callback){//FIRST CHECK IN REGISTRATION
 		
 		if(data==null) return callback('Reading name from db error!');
 		
@@ -91,11 +22,99 @@ class dbHandler{
 		});
 	};
 	
-	searchAd(data,id,callback){
+	checkForMail(data,callback){//SECOND CHECK IN REGISTRATION
+		
+		if(data == null) return callback('Wrong mail input!');
+		
+		let sql = "SELECT iduser FROM usersdb.users WHERE usersdb.users.email='"+data+"'";
+		
+		connection.query(sql,function(err,results,fields){
+
+			
+			if(results.length!=0) return callback('Mail is in use!');
+
+			return callback(null);
+		});
+	};
+	
+	addNewUser(data,callback){//FINALIZE REGISTRATION OF NEW USER
+		
+		let sql = 'INSERT INTO usersdb.users (username, password, email)VALUES (\''+data.usrname+'\',\''+data.password+'\',\''+data.email+'\');'
+		
+		connection.query(sql,function(err,results,fields){
+
+			
+			if(results.length==0) return callback('No changes made in db!');
+
+			return callback(null,results);
+		});
+		
+	};
+	//---------------------------------------------------------------------------------------------------------
+	checkLogIn(data,callback){//CHECKING FOR LOG IN
+
+		const sql = "SELECT * FROM usersdb.users WHERE usersdb.users.username ='"+data.name+"' && usersdb.users.password='"+data.pass+"';";
+		
+		connection.query(sql,function(err,results,fields){
+		
+		if(results.length){
+			
+			const stringRes = JSON.parse(JSON.stringify(results));//PARSING RESULTS FOR EASIER HANDLING
+			return callback(null,stringRes[0].iduser);
+			
+		}else{
+			
+			return callback('No such user!');
+			
+		}
+		});
+	};
+	//COLLECTING AD---------------------------------------------------------------------------------------------------------
+	checkPassword(data,callback){//CHECKING PASSWORD FOR COLLECTING MONEY
+		
+		const sql = 'SELECT * FROM usersdb.users WHERE users.password = "'+data+'";';
+		
+		connection.query(sql,function(err,results,fields){
+		
+		if(results.length == 0) return callback('Wrong password');
+		
+		return callback(null,results);
+		
+		});
+	};
+	
+	collectCoins(data,callback){//COLLECTING MONEY---HEARTS GO MINUS
+		
+		const sql = 'UPDATE usersdb.users SET hearts = hearts  - (SELECT SUM(likes) FROM usersdb.ads WHERE users.iduser='+data+' AND users.iduser = ads.iduser AND ads.active = 1) WHERE users.iduser='+data+';';
+		
+		connection.query(sql,function(err,results,fields){
+			
+			if(results.length == 0) return callback('Error collecting coins!');
+			
+			return callback(null,results);
+			
+		});
+		
+	};
+	
+	deactAd(data,callback){//AFTER COLLECTING DEACTIVATION OF AD
+		
+		const sql = 'UPDATE usersdb.ads SET active = 0 WHERE ads.iduser = '+data+' AND ads.active = 1;';
+		
+		connection.query(sql,function(err,results,fields){
+			
+			if(results.lentgh ==0) return callback('Error deactivation ad!');
+			
+			return callback(null,results);
+		});
+		
+	};
+	//LIKING AD---------------------------------------------------------------------------------------------------------
+	searchAd(data,callback){//SEARCHING AD FOR LIKING
 		
 		if(data==null) return callback('Invalid search input!');
 		
-		const sql = "SELECT * FROM usersdb.ads,usersdb.users WHERE active='1' AND ads.name='"+data+"' AND ads.iduser = users.iduser AND users.iduser != "+id+"";
+		const sql = "SELECT * FROM usersdb.ads,usersdb.users WHERE active='1' AND ads.name='"+data.name+"' AND ads.iduser = users.iduser AND users.iduser != "+data.id+"";
 		
 		connection.query(sql,function(err,results,fields){
 				
@@ -107,11 +126,11 @@ class dbHandler{
 		
 	};
 	
-	likeAd(data,userid,callback){
+	insertLike(data,callback){//LIKING AD--ADDING TO TABLE OF LIKES
 		
 		if(data==null) return callback('Invalid like input!');
 		
-		const sql = "INSERT INTO usersdb.likes (amount,userid,adsid)VALUES ("+data.amount+", "+userid+","+data.selected+");"
+		const sql = "INSERT INTO usersdb.likes (amount,userid,adsid)VALUES ("+data.amount+", "+data.id+","+data.selected+");"
 			
 		connection.query(sql,function(err,results,fields){
 				
@@ -126,7 +145,7 @@ class dbHandler{
 		});
 	};
 	
-	updateLikes(data,callback){
+	updateLikes(data,callback){//ADDING LIKES TO SELECTED AD
 		
 		const sql = "UPDATE usersdb.ads SET likes = likes + "+data.amount+" WHERE idad = "+data.selected+";";
 		
@@ -143,7 +162,21 @@ class dbHandler{
 		});
 	};
 	
-	searchForUser(data,callback){
+	likeAd(data,callback){//LIKING AD--HEARTS GO PLUS
+		
+		const sql = 'UPDATE usersdb.users SET hearts = hearts  + '+data.amount+' WHERE users.iduser = '+data.id+';';
+		
+		connection.query(sql,function(err,results,fields){
+			
+			if(results.length == 0) return callback('Error collecting coins!');
+			
+			return callback(null,results);
+			
+		});
+		
+	};	
+	//---------------------------------------------------------------------------------------------------------
+	searchForUser(data,callback){//SEARCHING USER INFORMATIONS FROM SECURE PAGE
 		
 		const sql = "SELECT * FROM usersdb.users WHERE users.iduser = "+data+";";
 		
@@ -159,6 +192,7 @@ class dbHandler{
 
 		});
 	};
+	//---------------------------------------------------------------------------------------------------------
 };
 
 module.exports = new dbHandler();
